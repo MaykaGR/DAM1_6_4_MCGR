1941a25d106f2c7b97d8ee6d2ebbf7b215bbb59c
MaykaGR <mcgonzalez@iesrafaelalberti.es>
2022-01-24 00:19:30 +0100

    update

--- ej6_4/build.gradle.kts
+++ ej6_4/build.gradle.kts
@@ -1,8 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.dokka.gradle.DokkaTask
+import java.net.URL
+
 
 plugins {
     kotlin("jvm") version "1.5.31"
     application
+    id("org.jetbrains.dokka") version "1.6.10"
 }
 
 group = "me.mayka"
@@ -14,6 +18,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.10")
 }
 
 tasks.test {
@@ -23,6 +28,22 @@ tasks.test {
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
+
+tasks.withType<DokkaTask>().configureEach {
+    dokkaSourceSets {
+        named("main") {
+            moduleName.set("Dokka Gradle Example")
+            includes.from("Module.md")
+            sourceLink {
+                localDirectory.set(file("src/main/kotlin"))
+                remoteUrl.set(URL("https://github.com/MaykaGR/DAM1_6_4_MCGR.git"
+                ))
+                remoteLineSuffix.set("#L")
+            }
+        }
+    }
+}
+
 
 application {
     mainClass.set("MainKt")
